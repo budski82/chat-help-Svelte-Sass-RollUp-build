763bc032b0e2edbbfc5b48275fff523ecdc89d0b
William Rutkowski <budski82@users.noreply.github.com>
2023-07-20 16:13:32 -0400

    1st javascript

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<!-- src/App.svelte -->
+<script>
+	let log = console.log
+	let name = `SvelteMan and here to Server`
+</script>
+
+<style lang="scss">
+
+	div.q{background:linear-gradient(
+		-45deg,
+		 #346 20%,
+		 #0000 50%,
+		#788 80%);
+		}
+	h1{color:purple;}
+
+</style>
+
+<div class="q">
+	<h1>Hello {name}! Buggerhead</h1>
+</div>
