0239088c09d969fa5b632192c0e43733dad821a1
William Rutkowski <budski82@users.noreply.github.com>
2023-07-20 16:22:11 -0400

    ff

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 // rollup.config.js
-// rollup.config.js
 import svelte from 'rollup-plugin-svelte';
-import css from 'rollup-plugin-css-only';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
